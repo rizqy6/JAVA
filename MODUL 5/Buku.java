d4853c0c365edecdff48af47a1da3152fcd4fd6c
fisaforsuccess <fisaforsuccess@gmail.com>
2022-10-10 10:26:21 +0700

    Perubahan Buku.java

--- MODUL 5/Buku.java
+++ MODUL 5/Buku.java
@@ -5,25 +5,15 @@ public class Buku {
     int cetakanKe;
     double hargaJual;
 
-    public Buku(String namaPengarang, String judulBuku,int tahunTerbit) {
+    public Buku(String namaPengarang, String judulBuku,int tahunTerbit, int cetakanKe, double hargaJual) {
         this.namaPengarang = namaPengarang;
         this.judulBuku = judulBuku;
         this.tahunTerbit = tahunTerbit;
-    }
-
-    public Buku( int cetakanKe){
         this.cetakanKe = cetakanKe;
-    }
-
-    public Buku(double hargaJual){
         this.hargaJual = hargaJual;
     }
+
     
-    public void 
-    fCetak(){
-
-    }
-
     public void info(){
         System.out.println("Nama Pengarang: " + namaPengarang + "\njudul Buku: " +judulBuku + "\nTahun Terbit: " + tahunTerbit + "\nCetakan Ke: " + cetakanKe + "\nHarga Jual: " + hargaJual);
     }
--- MODUL 5/BukuAccess.java
+++ MODUL 5/BukuAccess.java
@@ -1,6 +1,33 @@
 public class BukuAccess {
     public static void main(String[] args){
-        Buku a = new Buku("Abdi", "Bunga",1);
+        Buku a = new Buku("Abdi", "Bunga",1,1, 2000);
         a.info();
+
+        Buku b = new Buku("Abdi", "Bunga",1,1, 2000);
+        b.info();
+
+         Buku c = new Buku("Abdi", "Bunga",1,1, 2000);
+        c.info();
+
+        Buku d = new Buku("Abdi", "Bunga",1,1, 2000);
+        d.info();
+
+        Buku e = new Buku("Abdi", "Bunga",1,1, 2000);
+        e.info();
+
+        Buku f = new Buku("Abdi", "Bunga",1,1, 2000);
+        f.info();
+
+        Buku g = new Buku("Abdi", "Bunga",1,1, 2000);
+        g.info();
+
+        Buku h = new Buku("Abdi", "Bunga",1,1, 2000);
+       h.info();
+
+        Buku i = new Buku("Abdi", "Bunga",1,1, 2000);
+        i.info();
+
+        Buku j = new Buku("Abdi", "Bunga",1,1, 2000);
+        j.info();
     }
 }
