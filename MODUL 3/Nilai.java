32595b48f13b3e303322dc3154d00b17a64ffbc1
fisaforsuccess <fisaforsuccess@gmail.com>
2022-10-25 11:37:12 +0700

    s

--- MODUL 3/Nilai.java
+++ MODUL 3/Nilai.java
@@ -3,11 +3,13 @@ public class Nilai{
     int nilaiUAS;
     int nilaiTugas;
 
+    //void
     public void nilai(int nilaiUTS, int nilaiUAS, int nilaiTugas){
         double nilaiTotal = (nilaiTugas + nilaiUAS + nilaiUTS)/3;
         System.out.println(nilaiTotal);
     }
 
+    //nonvoid
     public double biji(int nilaiUTS, int nilaiUAS, int nilaiTugas){
         double nilaiTotal = (nilaiTugas + nilaiUAS + nilaiUTS)/3;
         return nilaiTotal;
