3a9cf1b308da1619d4fa856649a85c9e11a69ecc
fisaforsuccess <fisaforsuccess@gmail.com>
2022-11-28 10:35:34 +0700

    baruu

--- MODUL 7/KaryawanDemo.java
+++ MODUL 7/KaryawanDemo.java
@@ -9,6 +9,7 @@ public class KaryawanDemo {
         // System.out.println(k1.getGaji());
         // System.out.println(k1.getUsia());
 
+        //object satu
         Manager k2 = new Manager();
 
         k2.setNama("Rizqy");
@@ -28,5 +29,96 @@ public class KaryawanDemo {
         System.out.println("Alamat Manager: " + k2.getAlamat()); 
         System.out.println("Jam Kerja Manager: " + k2.GetJamKerja());
         System.out.println("Gaji Manager: " + k2.getGajiManager());
+
+
+        System.out.println("----------------------------------------------------------------");
+        //object dua
+        Manager k3 = new Manager();
+
+        k3.setNama("aji");
+        k3.setGaji(2600000);
+        k3.setUsia(20);
+
+
+        k3.setNamaManager("aji");
+        k3.setAlamat("solo");
+
+        System.out.println("Nama Karyawan: " + k3.getNama());
+        System.out.println("Gaji Karyawan: " + k3.getGaji());
+        System.out.println("Usia Karyawan: " + k3.getUsia());
+
+        System.out.println("Nama Manager: " + k3.getNamaManager());
+        System.out.println("Usia Manager: " + k3.getUsiaManager());
+        System.out.println("Alamat Manager: " + k3.getAlamat()); 
+        System.out.println("Jam Kerja Manager: " + k3.GetJamKerja());
+        System.out.println("Gaji Manager: " + k3.getGajiManager());
+
+        System.out.println("----------------------------------------------------------------");
+        //object tiga
+        Manager k4 = new Manager();
+
+        k4.setNama("bawa");
+        k4.setGaji(2300000);
+        k4.setUsia(21);
+
+
+        k4.setNamaManager("bawa");
+        k4.setAlamat("bali");
+
+        System.out.println("Nama Karyawan: " + k4.getNama());
+        System.out.println("Gaji Karyawan: " + k4.getGaji());
+        System.out.println("Usia Karyawan: " + k4.getUsia());
+
+        System.out.println("Nama Manager: " + k4.getNamaManager());
+        System.out.println("Usia Manager: " + k4.getUsiaManager());
+        System.out.println("Alamat Manager: " + k4.getAlamat()); 
+        System.out.println("Jam Kerja Manager: " + k4.GetJamKerja());
+        System.out.println("Gaji Manager: " + k4.getGajiManager());
+
+        System.out.println("----------------------------------------------------------------");
+        //object empat
+        Manager k5 = new Manager();
+
+        k5.setNama("harul");
+        k5.setGaji(2900000);
+        k5.setUsia(24);
+
+
+        k5.setNamaManager("harul");
+        k5.setAlamat("Jogja");
+
+        System.out.println("Nama Karyawan: " + k5.getNama());
+        System.out.println("Gaji Karyawan: " + k5.getGaji());
+        System.out.println("Usia Karyawan: " + k5.getUsia());
+
+        System.out.println("Nama Manager: " + k5.getNamaManager());
+        System.out.println("Usia Manager: " + k5.getUsiaManager());
+        System.out.println("Alamat Manager: " + k5.getAlamat()); 
+        System.out.println("Jam Kerja Manager: " + k5.GetJamKerja());
+        System.out.println("Gaji Manager: " + k5.getGajiManager());
+
+        System.out.println("----------------------------------------------------------------");
+        //object lima
+        Manager k6 = new Manager();
+
+        k6.setNama("hyu");
+        k6.setGaji(2700000);
+        k6.setUsia(17);
+
+
+        k6.setNamaManager("hyu");
+        k6.setAlamat("karanganyar");
+
+        System.out.println("Nama Karyawan: " + k6.getNama());
+        System.out.println("Gaji Karyawan: " + k6.getGaji());
+        System.out.println("Usia Karyawan: " + k6.getUsia());
+
+        System.out.println("Nama Manager: " + k6.getNamaManager());
+        System.out.println("Usia Manager: " + k6.getUsiaManager());
+        System.out.println("Alamat Manager: " + k6.getAlamat()); 
+        System.out.println("Jam Kerja Manager: " + k6.GetJamKerja());
+        System.out.println("Gaji Manager: " + k6.getGajiManager());
+
+        
     }
 }
